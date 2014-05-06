0ca5d9f0225fe633a4f77e82642e860ebd2935cc
gabriel-dehan <dehan.gabriel@gmail.com>
2014-05-06 10:53:04 +0200

    Trying to make it work with heroku & yaml

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'yaml'
 APP_ROOT = Pathname.new(File.expand_path(File.join(File.dirname(__FILE__), '..')))
 
 # loads the yml database config file
-DB_CONFIG = YAML::load(File.open(File.join(File.dirname(__FILE__),'database.yml')))
+DB_CONFIG = YAML::load(ERB.new(File.read(File.join(File.dirname(__FILE__),'database.yml'))).result)
 
 # gets the path of the database
 DB_PATH =  APP_ROOT.join(DB_CONFIG["development"]["database"])
